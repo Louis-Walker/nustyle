19631268f133a3fe665d91a81b8836d23582a243
Louis Walker <moshi.flashstep@gmail.com>
2022-06-22 23:13:20 +0100

    Added err for no port in env var

--- playlist.go
+++ playlist.go
@@ -37,9 +37,9 @@ func NewPlaylist(ctx context.Context, redirectURL string) (*Playlist, error) {
 	go func() {
 		port := os.Getenv("PORT")
 
-		// if port == "" {
-		// 	port = "8080" // throw error instead once local dev fixed
-		// }
+		if port == "" {
+			log.Fatalf("No PORT specified!")
+		}
 
 		fmt.Println("Listening on port: " + port)
 		err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
